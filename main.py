63707df23fa9a4f8587122862c7fd44a8f77c060
lukexyzz <lukecarter382@gmail.com>
2026-01-01 01:31:09 +0000

    added functions : get_book_text (opens and r file), get_num_words (sum of words in text)

---
+++ main.py
@@ -0,0 +1,19 @@
+from stats import get_num_words, character_count
+
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
+    
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+
+
+main()
