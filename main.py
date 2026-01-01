d578288e3c98b8375baeab7a1d58b8e2320f37b1
lukexyzz <lukecarter382@gmail.com>
2026-01-01 01:34:00 +0000

    feat: character_count, sum of each character

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    characters = character_count(text)
     print(f"Found {num_words} total words")
+    print(f"list of characters and how many times they appear {characters}")
     
 
 
--- stats.py
+++ stats.py
@@ -2,3 +2,11 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
+def character_count(text):
+    counts = {}
+    for i in text.lower():
+        if i not in counts:
+            counts[i] = 1
+        else:
+            counts[i] += 1
+    return counts   
